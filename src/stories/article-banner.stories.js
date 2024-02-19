27364f166d94aa5c5eb8f63e5202fdda5c724a0d
Viktor Fomin <viktor.fomin@onlyoffice.com>
2024-02-20 02:16:16 +0300

    Init story

---
+++ src/stories/article-banner.stories.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { Layout } from "../components/Layout";
+
+import lngs from "../../languages.json";
+
+const options = lngs.map((t) => t.shortKey);
+
+export default {
+  title: "Example/ArticleBanner",
+  component: Layout,
+  parameters: {
+    layout: "fullscreen",
+  },
+  argTypes: {
+    campaignName: {
+      control: {
+        type: "select",
+      },
+      options: ["CreateDocSpace", "FormGallery"],
+    },
+    language: {
+      control: {
+        type: "select",
+      },
+      options,
+    },
+  },
+};
+
+const Template = (args) => <Layout {...args} />;
+
+export const Default = Template.bind({});
+
+Default.args = {
+  campaignName: "CreateDocSpace",
+  language: "en",
+};
