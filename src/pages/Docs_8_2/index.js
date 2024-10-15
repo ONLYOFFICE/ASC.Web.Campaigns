8201409d67970ea0d95d43c38b9d2a9aac761722
Irina Tiulneva <irina.malysheva@onlyoffice.com>
2024-10-15 17:42:48 +0300

    TopBar: add Docs 8.2 released banner.

---
+++ src/pages/Docs_8_2/index.js
@@ -0,0 +1,65 @@
+import * as React from "react";
+import { graphql } from "gatsby";
+import { Trans, useTranslation } from "gatsby-plugin-react-i18next";
+import "../../styles/base.css";
+import "./index.css";
+
+const IndexPage = () => {
+  const {
+    t,
+    i18n: { language },
+  } = useTranslation("DocSpace_2_6");
+
+  const LinkHrefDocSpace_2_6 = t("Banner_Docs_8_2_Link");
+
+  return (
+    <div className="docs_8_2">
+      <div className={"advent-announce advent-mobile-hide " + language}>
+        <a
+          target="_blank"
+          href={LinkHrefDocSpace_2_6}
+          rel="noreferrer noopener"
+        >
+          <div className="advent-announce-text">
+            <div>
+              <Trans i18nKey="Banner_Docs_8_2_TextDesktop">
+                <strong>ONLYOFFICE Docs 8.2 released:</strong> PDF collaborative editing, revamped interface, optimized performance, and more
+              </Trans>
+            </div>
+          </div>
+        </a>
+      </div>
+      <div className={"advent-announce advent-desktop-hide " + language}>
+        <a
+          target="_blank"
+          href={LinkHrefDocSpace_2_6}
+          rel="noreferrer noopener"
+        >
+          <div className="advent-announce-text">
+            <strong>
+              <Trans i18nKey="Banner_Docs_8_2_TextMob">
+                Meet ONLYOFFICE Docs 8.2
+              </Trans>
+            </strong>
+          </div>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default IndexPage;
+
+export const query = graphql`
+  query {
+    locales: allLocale {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`;
